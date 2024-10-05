c92043b9f45a23b6ffdd018a8a70328dfec28d98
Aditya Kumar Tiwari <adityak8340@gmail.com>
2024-10-06 01:08:07 +0530

    ...

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+function Table(a, fnToCall){
+    console.log('Hello World');
+    for(let i = 1; i <= 10; i++) {
+        fnToCall(a, i);
+    }
+}
+
+function PrintTable(a,i) {
+    console.log(a, " * ", i, " = ", a*i);
+}
+
+Table(2, PrintTable); //callbacks
