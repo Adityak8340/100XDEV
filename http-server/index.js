827e496e72e3f3acc5057dc78008b9236f1d5396
Aditya Kumar Tiwari <adityak8340@gmail.com>
2024-10-07 01:10:39 +0530

    ---

---
+++ http-server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.use(bodyParser.json());
+
+app.post("/conversation", function(req, res) {
+    console.log(req.headers);
+    console.log(req.body);
+    res.send({ 
+        msg: "This is a POST request",
+        status: "success",
+        calc: 1 + 1,
+        answer: 2,
+        hey: "1+1=2??"
+    });
+    });
+
+app.listen(port, function() {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
