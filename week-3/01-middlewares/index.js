944b019fce011b51fcd2da0b6a53cc06d2e21604
Aditya Kumar Tiwari <adityak8340@gmail.com>
2024-10-20 14:38:05 +0530

    Middlewares

---
+++ week-3/01-middlewares/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/health-checkup", function(req, res) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
+
+    if (username === "Aadi" && password === "1234") {
+        if (kidneyId == 1  || kidneyId == 2) {
+            res.json({
+                msg: "Kidney is healthy"
+        })
+        }
+    } 
+    res.status(400).json({"msg": "Somethings up with your inputs!"});
+});
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+});
